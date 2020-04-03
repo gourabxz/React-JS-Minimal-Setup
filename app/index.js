c1bb76b502c79c496480da624a1d52ff4b2c6bfb
gourabxz <gourabxz@gmail.com>
2020-04-03 20:06:21 +0600

    removed unnecessary codes

--- app/index.js
+++ app/index.js
@@ -5,17 +5,14 @@ import './index.css';
 
 import TestPage from './Pages/TestPage';
 
-function Clock(Props){
-    let [myData, setMyData] = useState();
-
-    function toggle() {
-        setMyData(!myData);
-    }
-    
+function Clock(){
+    let hello = "Hello React"
     return(
     <div>
+        <h1>{hello}</h1>
     </div>
     )
 }
 
+ReactDOM.render(<Clock />,document.getElementById('root'))
 
